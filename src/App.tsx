b41903b3a86670a2827adc4e91ca8ed646537bf8
FranLop24 <franlopbri@gmail.com>
2026-06-09 12:58:37 -0600

    feat: Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Home } from "./components/Home";
+
 export default function App() {
   return (
+    <>
     <h1 className="text-3xl w-full text-center py-2">
       Hello world 🚀🚀
     </h1>
+    <hr />
+    <Home />
+    </>
   )
 }
