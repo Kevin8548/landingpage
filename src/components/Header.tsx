3b6b773974f16e5724ea7795023f4da708c4d03d
Esme Munoz <esmeralda.padilla.com@gmail.com>
2026-06-10 15:04:50 -0600

    Agregando header xd

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+// header
+export default function Header() {
+  return (
+    <header className="w-full h-16 bg-gray-800 text-white flex items-center justify-center">
+      <h1 className="text-xl font-bold">My App</h1>
+    </header>
+  )
+}
